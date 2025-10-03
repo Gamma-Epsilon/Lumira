23d8e1c80d51e739a19760ca8df45db299a2d7d7
Gamma-Epsilon <tymahob@yandex.ru>
2025-10-03 19:33:51 +0300

    commit

--- Telegram_Bot/LumiraEduBot.py
+++ Telegram_Bot/LumiraEduBot.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
-from config import BOT_TOKEN
+from config.Bot_config import BOT_TOKEN
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
